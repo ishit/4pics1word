c1b61a9948c3ff411a5350ac28f9067406e799a6
Ishit Mehta <mehta.ib@gmail.com>
2016-03-17 19:21:52 +0530

    Change migration syntax for rials 4.x

--- db/migrate/20140905211709_add_score_to_users.rb
+++ db/migrate/20140905211709_add_score_to_users.rb
@@ -1,6 +1,6 @@
 class AddScoreToUsers < ActiveRecord::Migration
   def change
-  	add_column (:users, :score, :integer. :default => 0)
-	add_column (:users, :name, :string)
+  	add_column :users, :score, :integer. default: 0
+	add_column :users, :name, :string
   end
 end
